1eea6d67509831a56686cc2dcf45477a1c00e8a2
jphaugla <jasonhaugland@gmail.com>
2020-06-08 09:42:09 -0500

    add file load python command

---
+++ src/fileLoad.py
@@ -0,0 +1,23 @@
+import os
+from redisearch import Client, TextField, NumericField, Query
+
+client = Client('myIndex', 'redis', 6379)
+print("connect successful")
+# Creating the index definition and schema
+# client.drop_index();
+# client.create_index([TextField('title', weight=5.0), TextField('body'), TextField('phonetic')])
+base_directory = "data/"
+cnt = 0
+for (dirpath, dirnames, filenames) in os.walk(base_directory):
+    print("dirpath=" + dirpath)
+    for file in filenames:
+        print("file=" + file)
+        if ("txt" in file):
+            shortname = file.replace(".txt","")
+            print("shortname is" + shortname)
+            openname = dirpath + "/" + file
+            print("openname is " + openname)
+            f = open(openname)
+            fileText = f.read()
+            client.add_document(shortname, title=shortname, body=fileText, phoneticBody=fileText)
+
